c9b9128a6a46143d8b1719cb3a409ec1744a7b5d
Fred Bolder <bolder@muenzgalerie.de>
2025-01-20 17:13:18 +0100

    den ksero poso s agapo added

--- modules/script.js
+++ modules/script.js
@@ -659,6 +659,7 @@ function loadOtherIndex() {
   d.push("107 Γλυκά μου μάτια");
   d.push("108 Περνάς καλά");
   d.push("109 Ξανά");
+  d.push("110 Δεν ξέρω πόσο σ’ αγαπώ");
   input.value = d.join("\n");
 }
 
@@ -1305,7 +1306,7 @@ function surpriseMeClicked() {
     }
     if (isInt) {
       n = parseInt(value);
-      if (n < 1 || (n > Songs.numberOfSongs() && (n < 101 || n > 109))) {
+      if (n < 1 || (n > Songs.numberOfSongs() && (n < 101 || n > 110))) {
         n = 0;
       }
     }
